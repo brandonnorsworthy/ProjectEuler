a4d2fee688a986380a9e07acc2b98ff8d68acdb9
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-11-09 13:40:44 -0600

    Finished p004.py

--- python/p004.py
+++ python/p004.py
@@ -1,13 +1,21 @@
 #A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
-found = False
+foundnumber = 0
 for x in reversed(range(900,1000)):
     for y in reversed(range(900,1000)):
         number = x * y
-        print(str(number)[0:3], ' r', str(number)[6:2:-1], ' x', x, ' y', y)
-        found = True
+        
+        #print(str(number)[0:3], ' r', str(number)[6:2:-1], ' x', x, ' y', y)
+
+        str1 = str(number)[0:3]
+        str2 = str(number)[6:2:-1]
+
+        if(str1 == str2):
+            foundnumber = number
+            break
+
+    if foundnumber != 0:
         break
 
-    if found:
-        break
+print(foundnumber)
