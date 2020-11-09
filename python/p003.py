99d8da44f7eaa8af6edbfe67711a329025db01a9
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2020-11-09 13:23:56 -0600

    Finished p003.py

--- python/p003.py
+++ python/p003.py
@@ -1,36 +1,18 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
-def main():
-    number = 55
-    count = 2
+number = 600851475143
 
-    while True:
-        if number % count == 0:
-            if isPrimeNumber(count):
-                break
-        count += 1
-        if count >= number:
+for x in range(2, int(number/2)):
+    if number % x != 0:
+        continue
+    oppositex = int(number / x)
+    foundPrime = True
+    for y in range(2, int(oppositex/2) - 1):
+        if oppositex % y == 0:
+            foundPrime = False
             break
-        continue
-
-    print(number)
-
-def testPrime(factor):
-    for i in range(2, factor - 1):
-        print(factor, '%', i, '==', factor % i == 0)
-        if factor % i == 0:
-            return False
-    print('true')
-    return True
-
-def isPrimeNumber(number):
-    for iterator in range(number - 1, 2):
-        if number % iterator == 0:
-            return True
-        continue
-    return False
-
-
-if __name__ == "__main__":
-    main()
+    if(foundPrime == True):
+        print(oppositex)
+        break
+    continue
